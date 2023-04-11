659abf2df9f379159f876c53b40f68b574a9d695
tolgaand <tolgaand@yandex.com>
2023-04-11 17:21:32 +0300

    refactor: list tools in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,44 @@
-import Head from "next/head";
-import Image from "next/image";
+import {
+  Button,
+  Card,
+  CardBody,
+  Container,
+  SimpleGrid,
+  Text,
+} from "@chakra-ui/react";
+
+import NextLink from "next/link";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <Container maxWidth="100%" padding="20px 50px">
+      <SimpleGrid
+        spacing={4}
+        templateColumns="repeat(auto-fill, minmax(350px, 1fr))"
+      >
+        <Card>
+          <CardBody>
+            <Text fontWeight="bold" fontSize="xl" textTransform="uppercase">
+              Sprite Generator Tool
+            </Text>
+            <Text>
+              This tool allows you to generate a sprite image and CSS from a
+              collection of images. Upload your images and get the CSS code for
+              your sprite image.
+            </Text>
+            <Button
+              as={NextLink}
+              href="/sprite-generator"
+              marginTop="20px"
+              size="sm"
+              textTransform="uppercase"
+              fontWeight="bold"
+            >
+              View
+            </Button>
+          </CardBody>
+        </Card>
+      </SimpleGrid>
+    </Container>
+  );
 }
